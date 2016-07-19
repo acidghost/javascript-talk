6451cf962d8689d519ac84cb88efec747d6fa1f3
Andrea Jemmett <andreajemmett@gmail.com>
2016-07-20 00:53:30 +0200

    initial test

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+
+gulp.task('slides', function() {
+  spawn('pandoc', ['-s', 'slides.md', '-t', 'revealjs', '-o', 'slides.html']);
+});
+
+gulp.task('dev', function() {
+  gulp.watch('slides.md', ['slides'])
+});
+
+gulp.task('default', ['dev'])
